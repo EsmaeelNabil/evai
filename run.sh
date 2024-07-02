062b61ea4ae44182070234386e3a008dc9518763
Esmaeel Moustafa <esmaeel.moustafa@thermondo.de>
2024-07-02 16:31:28 +0200

    updates run command

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+if ! command -v ollama >/dev/null 2>&1; then
+    echo "Please install Ollama first https://ollama.com/"
+    exit 1
+fi
+
+if ! command -v docker >/dev/null 2>&1; then
+    echo "Please install Docker first https://docs.docker.com/get-docker/"
+    exit 1
+fi
+
+echo "Pulling llava:latest"
 ollama pull llava:latest &&
 
+echo "Creating qm model"
 ollama create qm &&
 
+echo "Running backend service"
 docker-compose up --build
